eb4f5b6bb75c4e3aad3a36b5103b1064d5c82f94
CooperSchahinger <cooperschahinger@gmail.com>
2023-03-14 14:36:58 +1030

    feat: add polymorphism animal example

---
+++ polymorphism/polymorphism/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        // Create new Animal object
+        Animal myAnimal = new Animal();
+        
+        // Create new Pig object
+        Animal myPig = new Pig();
+        
+        // Create new Monkay object
+        Animal myMonkey = new Monkey();
+        
+        
+        myAnimal.animalSound();
+        
+        myPig.animalSound();
+        
+        myMonkey.animalSound();
+    }
+}
---
+++ polymorphism/polymorphism/src/Monkey.java
@@ -0,0 +1,5 @@
+class Monkey extends Animal{
+    public void animalSound() {
+        System.out.println("The monkey says oohooohh ahhahh");
+    }
+}
